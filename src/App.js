3d957275416b00b0fe0b338af068dd380dfecbb5
Kaushik das <kaushik1641.cse19@chitkara.edu.in>
2023-01-24 15:18:49 +0530

    C02

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import { Routes, Route } from 'react-router-dom';
 import Register from './components/Register.js';
 import Edit from './components/Edit';
+import Details from './components/Details';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/register' element={<Register />} />
         <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/view/:id" element={<Details />} />
       </Routes>
     </>
   );
