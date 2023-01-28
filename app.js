2b586c50074278219ff4c43c9e4ba5db5c4ef9d1
Kaushik das <danishsawl6@gmail.com>
2023-01-28 20:37:39 +0530

    serv02

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const port = 3001;
-
+const cors = require('cors');
+const router = require("./routes/router");
 
 const users = require("./models/userSchema");
 require("./db/conct");
 
+app.use(cors())
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
